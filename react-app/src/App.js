917692471d283ea88559a18b49c5746c907bc7b9
hensell de la rosa <henseldelarosa@DESKTOP-DOB1QI3.localdomain>
2023-08-16 22:42:44 -0400

    set up preset

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -18,12 +18,14 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login" >
+          {/* <Route path="/login" >
             <LoginFormPage />
           </Route>
           <Route path="/signup">
             <SignupFormPage />
-          </Route>
+          </Route> */}
+
+          
         </Switch>
       )}
     </>
