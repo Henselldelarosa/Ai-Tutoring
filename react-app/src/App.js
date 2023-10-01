22ca0ba91b3bdfdc1a47801832f7ef11a51e61a4
hensell de la rosa <henseldelarosa@DESKTOP-DOB1QI3.localdomain>
2023-10-01 01:53:15 -0400

    navbar done

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,23 @@
 import React, {useState, useEffect} from "react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {Route, Switch} from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
-import LoginFormPage from "./components/LoginFormPage";
 import {authenticate} from "./store/session";
 import Navigation from "./components/Navigation";
 import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
 import Signup from "./pages/signup/Signup";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   const dispatch = useDispatch();
+  const sessionUser = useSelector(state => state.session.user)
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     dispatch(authenticate()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
   return (<>
-    <Navigation isLoaded={isLoaded}/> {
+    <Navbar sessionUser={sessionUser} isLoaded={isLoaded}/> {
     isLoaded && (<Switch>
       <Route path='/login'>
         <Login/>
---
+++ react-app/src/components/navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Navbar.css'
+import { useSelector } from 'react-redux'
+import DropDown from '../buttons/dropDown/DropDown'
+
+const Navbar = ({isLoaded, sessionUser}) => {
+
+  return (
+    <div className='navbar'>
+      <div className="navar__wrapper">
+
+        <div className="navar__left">
+          <img
+          src="images/LoginLogo.png"
+          alt=""
+          className="navar__left--logo"
+          />
+          <h1 className='navbar__left--title'>Course Correct</h1>
+        </div>
+
+        <div className="navbar__right">
+          <h1 className="navbar__right--username">{sessionUser?.username}</h1>
+          <DropDown/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
