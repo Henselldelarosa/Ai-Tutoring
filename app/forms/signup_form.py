7b2abe593ee7b37bd2e7aa39e69d6cd1c6b400bd
Wdn225 <104331376+WarrenNou@users.noreply.github.com>
2023-10-10 00:30:30 -0400

    Update signup_form.py

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,27 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, ValidationError
 from app.models import User
-
-
-def user_exists(form, field):
-    # Checking if user exists
-    email = field.data
-    user = User.query.filter(User.email == email).first()
-    if user:
-        raise ValidationError('Email address is already in use.')
-
-
-def username_exists(form, field):
-    # Checking if username is already in use
-    username = field.data
-    user = User.query.filter(User.username == username).first()
-    if user:
-        raise ValidationError('Username is already in use.')
-
 
 class SignUpForm(FlaskForm):
     username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+        'Username', validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self, field):
+        email = field.data
+        user = User.query.filter_by(email=email).first()
+        if user:
+            raise ValidationError('Email address is already in use.')
+
+    def validate_username(self, field):
+        username = field.data
+        user = User.query.filter_by(username=username).first()
+        if user:
+            raise ValidationError('Username is already in use.')
