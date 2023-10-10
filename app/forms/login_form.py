f02e758e0ef14e90a96804110add340c0bb37e24
Wdn225 <104331376+WarrenNou@users.noreply.github.com>
2023-10-10 00:28:59 -0400

    Update login_form.py

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -1,28 +1,85 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, ValidationError
-from app.models import User
+from flask_login import LoginManager, UserMixin, login_user, logout_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'your_database_uri'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 
-def user_exists(form, field):
-    # Checking if user exists
-    email = field.data
-    user = User.query.filter(User.email == email).first()
-    if not user:
-        raise ValidationError('Email provided not found.')
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def set_password(self, password):
+        self.password = password
+
+    def check_password(self, password):
+        return self.password == password
 
 
-def password_matches(form, field):
-    # Checking if password matches
-    password = field.data
-    email = form.data['email']
-    user = User.query.filter(User.email == email).first()
-    if not user:
-        raise ValidationError('No such user exists.')
-    if not user.check_password(password):
-        raise ValidationError('Password was incorrect.')
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired(), password_matches])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+    def validate_email(self, field):
+        email = field.data
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            raise ValidationError('No user found with this email address.')
+
+    def validate_password(self, field):
+        email = self.email.data
+        password = field.data
+        user = User.query.filter_by(email=email).first()
+        if user and not user.check_password(password):
+            raise ValidationError('Incorrect password.')
+
+
+@app.route('/')
+def home():
+    return 'Welcome to the Flask Login Example!'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        user = User.query.filter_by(email=email).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return 'Dashboard - You are logged in!'
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
