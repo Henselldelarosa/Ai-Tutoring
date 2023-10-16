e18a6e42030da98060387dcfcaf5148b1764f692
Wdn225 <104331376+WarrenNou@users.noreply.github.com>
2023-10-15 22:52:19 -0400

    Update login_form.py

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -1,47 +1,28 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
-from flask_login import LoginManager, UserMixin, login_user, logout_user
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'your_database_uri'
 
-# ... (definitions of user_exists, db, User, login_manager, password_matches)
+def user_exists(form, field):
+    # Checking if user exists
+    email = field.data
+    user = User.query.filter(User.email == email).first()
+    if not user:
+        raise ValidationError('Email provided not found.')
 
-# ... (User model definition and login_manager.user_loader)
 
-# Definition of the LoginForm using WTForms with email and password fields
+def password_matches(form, field):
+    # Checking if password matches
+    password = field.data
+    email = form.data['email']
+    user = User.query.filter(User.email == email).first()
+    if not user:
+        raise ValidationError('No such user exists.')
+    if not user.check_password(password):
+        raise ValidationError('Password was incorrect.')
 
-@app.route('/')
-def home():
-    return 'Welcome to the Flask Login Example!'
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        email = form.email.data
-        user = User.query.filter_by(email=email).first()
-        if user:
-            login_user(user)
-            return redirect(url_for('dashboard'))
-    return render_template('login.html', form=form)
-
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    return 'Dashboard - You are logged in!'
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('home'))
-
-if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired(), user_exists])
+    password = StringField('password', validators=[DataRequired(), password_matches])
