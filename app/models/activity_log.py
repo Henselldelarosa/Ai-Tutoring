c54e3cb6f63a252e3097c267ca9e709271c85e91
Wdn225 <104331376+WarrenNou@users.noreply.github.com>
2023-10-15 21:33:34 -0400

    Create activity_log.py

---
+++ app/models/activity_log.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from app import db
+
+class ActivityLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    activity_type = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<ActivityLog {self.id}>"
